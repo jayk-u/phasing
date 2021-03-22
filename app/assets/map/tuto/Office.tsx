15ea786f01da26725cfcf7affe7a93fb047ce2cf
jay cormier <jay.cormier@me.com>
2021-03-22 17:09:09 +0000

    added extra files to the tutorial map

---
+++ app/assets/map/tuto/Office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Office" tilewidth="32" tileheight="32" tilecount="2262" columns="78">
+ <image source="../../../../../../../Pictures/GuttyKreum_JapaneseOfficeInterior/Tilemap/MainTileMap.png" width="2496" height="928"/>
+</tileset>

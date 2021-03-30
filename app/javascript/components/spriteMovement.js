261ff7463c5cc1f63c68988d731f16cb7dc53a23
Nicolas Dumont <nicolas.dumont@essca.eu>
2021-03-30 04:47:41 +0200

    So, the character was being changed but the animations weren't (they had the same name as the previous one). My insurer didn't take this into account

--- app/javascript/components/spriteMovement.js
+++ app/javascript/components/spriteMovement.js
@@ -1,54 +1,54 @@
-const movementSprite = (game, character, cursors, status) => {
+const movementSprite = (game, character, cursors, counter, status) => {
   character.body.setVelocity(0);
-  if (status.minigame != "active") {
+  if (status.minigame != `active${counter}`) {
     if (cursors.left.isDown) {
       character.setVelocityX(-90);
-      character.anims.play("left", true);
+      character.anims.play(`left${counter}`, true);
       game.x = 1;
     } else if (cursors.right.isDown) {
       character.setVelocityX(90);
 
-      character.anims.play("right", true);
+      character.anims.play(`right${counter}`, true);
       game.x = 2;
     } else if (cursors.down.isDown) {
       character.setVelocityY(90);
 
-      character.anims.play("down", true);
+      character.anims.play(`down${counter}`, true);
       game.x = 3;
     } else if (cursors.up.isDown) // && character.body.touching.down 
     {
       character.setVelocityY(-90);
-      character.anims.play("up", true);
+      character.anims.play(`up${counter}`, true);
       game.x = 4;
     } else {
       character.setVelocityX(0);
       if (game.x === 1) {
-        character.anims.play("leftend");
+        character.anims.play(`leftend${counter}`);
       }
       else if (game.x === 2) {
-        character.anims.play("rightend");
+        character.anims.play(`rightend${counter}`);
       }
       else if (game.x === 3) {
-        character.anims.play("downend");
+        character.anims.play(`downend${counter}`);
       }
       else if (game.x === 4) {
-        character.anims.play("upend");
+        character.anims.play(`upend${counter}`);
       }
-      //character.anims.play("turn");
+      //character.anims.play(`turn${counter}`);
       }
   } else {
       character.setVelocityX(0);
       if (game.x === 1) {
-        character.anims.play("leftend");
+        character.anims.play(`leftend${counter}`);
       }
       else if (game.x === 2) {
-        character.anims.play("rightend");
+        character.anims.play(`rightend${counter}`);
       }
       else if (game.x === 3) {
-        character.anims.play("downend");
+        character.anims.play(`downend${counter}`);
       }
       else if (game.x === 4) {
-        character.anims.play("upend");
+        character.anims.play(`upend${counter}`);
       }
   }
 }

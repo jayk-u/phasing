39a4ef832269791ff2f1abeb32f6ebb768d4e0a5
Nicolas Dumont <nicolas.dumont@essca.eu>
2021-04-02 18:11:33 +0200

    I don't even know what I changed

--- app/javascript/scenes/play1.js
+++ app/javascript/scenes/play1.js
@@ -97,6 +97,21 @@ class Play1 extends Phaser.Scene {
         frameWidth: 32,
         frameHeight: 48,
       });
+    } else if (characterCounter === 3) {
+      this.load.spritesheet("character3", gameAssets.character3Sprite, {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
+    } else if (characterCounter === 4) {
+      this.load.spritesheet("character4", gameAssets.character4Sprite, {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
+    } else if (characterCounter === 5) {
+      this.load.spritesheet("character5", gameAssets.character5Sprite, {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
     }
     //End Sprite
 

f26f527d21b7a329382ae7a5d284c03562638236
Pierre Bodin <pro@pierrebodin.com>
2021-09-16 13:50:17 +0200

    added newmap

---
+++ app/assets/map/JapanCollection.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Newone" tilewidth="32" tileheight="32" tilecount="1260" columns="28">
+ <image source="../../../../../../Library/Mobile Documents/com~apple~CloudDocs/BreakingOut/Assets/GuttyKreum_OvergrownBackstreets_v1/Tilemap/MainTileMap.png" width="896" height="1440"/>
+</tileset>
---
+++ app/assets/map/JapaneseHouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="MainTileMap" tilewidth="32" tileheight="32" tilecount="1365" columns="65">
+ <image source="MainTileMap.png" width="2080" height="672"/>
+</tileset>
